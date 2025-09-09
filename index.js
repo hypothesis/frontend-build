32db19d2c604468e4199fbbeb48cf18e7d0ac96d
Robert Knight <robertknight@gmail.com>
2025-09-09 11:25:58 +0100

    Support Tailwind v4 in CSS build
     - Support using Tailwind v3 or v4 in `buildCSS`
    
     - Support enabling Tailwind without passing JS configuration. For
       Tailwind v4 the PostCSS plugin no longer takes a configuration object
       as an argument.
    
     - Update TS module resolution settings to work with Tailwind v4's
       built-in types
    
     - Simplify the build pipeline when SASS preprocessing is not needed by
       limiting the use of SASS to inputs with a `.scss` extension. Removing
       SASS from the pipeline will simplify adopting Tailwind v4, eg. by
       avoiding having SASS try to process @import rules.
    
     - Rename `lib/{sass.js => css.js}` to reflect that SASS use is now
       optional
    
     - Make the use of autoprefixer optional. Tailwind v4's documentation
       recommends avoiding it as Tailwind can handle this.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export { default as log } from 'fancy-log';
 
+export { buildCSS } from './lib/css.js';
 export { buildJS, watchJS } from './lib/rollup.js';
-export { buildCSS } from './lib/sass.js';
 export { generateManifest } from './lib/manifest.js';
 export { runTests } from './lib/tests.js';
 export { run } from './lib/run.js';
